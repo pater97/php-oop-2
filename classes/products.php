d3ea548bb5ad16b68a85acc113504ea7d0c6515b
pater97 <giuseppepater997@gmail.com>
2021-12-21 16:33:23 +0100

    implementate classi figle prodotto

--- classes/products.php
+++ classes/products.php
@@ -17,6 +17,46 @@
 
  }
 
- $ascia = new product(rand(0,100),'ascia','garden',19.99,true);
- $scolapasta = new product(rand(0,100),'scolapasta','kitchen',9.99,true);
- $guanti = new product(rand(0,100),'guanti','outdoor',4.99,false);
+ class gardenProduct extends product{
+     public $power; //eletric/manual/water
+     public $application; //wood,grass,ground
+     public $genre; //knife,to dig,cleaner
+
+     function __construct($code,$name,$type,$price,$avalabile=false,$power,$application,$genre){
+         parent:: __construct($code,$name,$type,$price,$avalabile=false);
+         $this->power = $power;
+         $this->application = $application;
+         $this->genre = $genre;
+     }
+ }
+
+
+ class fashionProduct extends product{
+     public $categories; //hoody,pants,giacet,gloves
+     public $season; //winter,summer
+     public $materials; //denim,poliester,wool,cashmire
+
+     function __construct ($code,$name,$type,$price,$avalabile=false,$categories,$season,$materials){
+        parent:: __construct($code,$name,$type,$price,$avalabile=false);
+        $this->categories = $categories;
+        $this->season = $season;
+        $this->materials = $materials;
+    }
+ }
+
+ class kitchenProduct extends product{
+     public $whatCook; //sweet or salty
+     public $use; //oven,micronde,stove
+     public $kind; //gadget/elettrodomestico
+
+     function __construct($code,$name,$type,$price,$avalabile=false,$whatCook,$use,$kind){
+        parent:: __construct($code,$name,$type,$price,$avalabile=false);
+        $this->whatCook = $whatCook;
+        $this->use = $use;
+        $this->kind = $kind;
+    }
+ }
+
+ $ax = new gardenProduct(rand(0,100),'ax','garden',19.99,true,'manual','wood','knife');
+ $gloves = new kitchenProduct(rand(0,100),'gloves','outdoor',4.99,false,'gloves','winter','wool');
+ $colander = new fashionProduct(rand(0,100),'colander','kitchen',9.99,true,'salty','stove','plastic');
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/classes/products.php';
 require_once __DIR__ . '/classes/users.php';
 require_once __DIR__ . '/function/function.php';
 
-dd($user_1,$user_2,$user_3);
-// dd($ascia,$guanti,$scolapasta);
+
+dd($ax,$gloves,$colander,$user_1,$user_2,$user_3);
 
 
